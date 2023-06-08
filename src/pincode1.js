9fd433b3f3640abe3bc73e6f4e653e961e7b7459
ks0510 <kshaikh051999@gmail.com>
2023-06-08 15:42:39 +0530

    Added javascript program for validating wrong pincode

--- src/pincode1.js
+++ src/pincode1.js
@@ -1,6 +1,17 @@
+//To check pincode using regex.
 let pincodeRegex = RegExp('^[0-9]{6}$');
-let pincodeCheck = pincodeRegex.test(400088);
+let pincodeCheck = pincodeRegex.test("400088");
 if(pincodeCheck){
+    console.log("Pincode is valid");
+}
+else{
+    console.log("Pincode is not valid");
+}
+//UC2 Restrict to take alphabet at start
+
+let pincodeCheck1 = pincodeRegex.test("A400088");
+
+if(pincodeCheck1){
     console.log("Pincode is valid");
 }
 else{
