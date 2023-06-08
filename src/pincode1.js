bd23889ae5a624b2c8de206b04846c6fb82ea0a9
ks0510 <kshaikh051999@gmail.com>
2023-06-08 15:34:14 +0530

    Added javascript and html file for validate pincode using regex

---
+++ src/pincode1.js
@@ -0,0 +1,8 @@
+let pincodeRegex = RegExp('^[0-9]{6}$');
+let pincodeCheck = pincodeRegex.test(400088);
+if(pincodeCheck){
+    console.log("Pincode is valid");
+}
+else{
+    console.log("Pincode is not valid");
+}
