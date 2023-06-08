1a1a3448a64054b727f52eb855b4981eeca7b05a
ks0510 <kshaikh051999@gmail.com>
2023-06-08 15:47:02 +0530

    Added javascript program for restricting pincode to take alphabet at end

--- src/pincode1.js
+++ src/pincode1.js
@@ -8,10 +8,20 @@ else{
     console.log("Pincode is not valid");
 }
 //UC2 Restrict to take alphabet at start
-
 let pincodeCheck1 = pincodeRegex.test("A400088");
 
 if(pincodeCheck1){
+    console.log("Pincode is valid");
+}
+else{
+    console.log("Pincode is not valid");
+}
+
+//UC3 Restrict to take alphabet and special characters at end
+
+let pincodeCheck2 = pincodeRegex.test("400088B");
+
+if(pincodeCheck2){
     console.log("Pincode is valid");
 }
 else{
